664160e0c22bc071512c819e32f2573eb067b5e9
TahsinAyman <mail4tahsin@gmail.com>
2022-07-13 07:48:15 +0600

    init

---
+++ src/main/java/com/mura/eduline/EdulineApplication.java
@@ -0,0 +1,13 @@
+package com.mura.eduline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdulineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdulineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mura/eduline/EdulineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mura.eduline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdulineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
